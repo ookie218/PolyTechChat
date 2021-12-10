30b536e3eae30c1ebe7b8aee8ab871309e6b85c6
Michael "Ookie" Ebron <ookieebron@gmail.com>
2021-12-10 16:14:05 -0500

    Initial commit

---
+++ app/src/main/java/com/ookie/polytechchat/SignUp.kt
@@ -0,0 +1,11 @@
+package com.ookie.polytechchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
