1a8bdbe75d6984d28bdafe2b3069ded3b3a93035
Michael "Ookie" Ebron <ookieebron@gmail.com>
2021-12-10 16:35:15 -0500

    Updated XML for SignUp and Login; Created Splash page with school logo

---
+++ app/src/main/java/com/ookie/polytechchat/SplashPage.kt
@@ -0,0 +1,24 @@
+package com.ookie.polytechchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashPage : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 4000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_page)
+
+        Handler().postDelayed({ //Intent goes from Splash to Main
+            val splashIntent = Intent(this@SplashPage, MainActivity::class.java)
+            startActivity(splashIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())//Pass in how long it goes for
+
+
+    }
+}
