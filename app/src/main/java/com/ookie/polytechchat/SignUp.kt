f23a5bc784ca13bc5fc2944fab0371ac58ba605e
Michael "Ookie" Ebron <ookieebron@gmail.com>
2021-12-20 04:50:33 -0500

    User constructor and class has been updated to account for differentiating the students and faculty (updated)
    also updated the login and sign up UI

--- app/src/main/java/com/ookie/polytechchat/SignUp.kt
+++ app/src/main/java/com/ookie/polytechchat/SignUp.kt
@@ -3,6 +3,7 @@ package com.ookie.polytechchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -18,8 +19,11 @@ class SignUp : AppCompatActivity() {
     private lateinit var editEmail : EditText
     private lateinit var editPassword : EditText
     //private lateinit var editIdNumber : EditText
-    private lateinit var title : String
+    private lateinit var role : String
+    private lateinit var year : String
     private lateinit var editDepartment : EditText
+    private lateinit var studentRadioButton : RadioButton
+    private lateinit var facultyRadioButton : RadioButton
     private lateinit var btnSignUp : Button
 
     private lateinit var mAuth: FirebaseAuth
@@ -39,7 +43,23 @@ class SignUp : AppCompatActivity() {
         editPassword = findViewById(R.id.editText_password)
         editDepartment = findViewById(R.id.editText_Department)
         //editIdNumber = findViewById(R.id.idNumber) The user doesn't need to see this
+        studentRadioButton = findViewById(R.id.studentRadioButton)
+        facultyRadioButton = findViewById(R.id.facultyRadioButton)
         btnSignUp = findViewById(R.id.btn_signup)
+
+
+        //Gray out editText unless faculty is checked
+        if (facultyRadioButton.isChecked) {
+            role = "Faculty"
+            year = ""
+            editDepartment.isEnabled
+
+        } else {
+            role = "Student"
+            year = (2022..2026).random().toString()
+            editDepartment.isEnabled = false
+        }
+
 
         btnSignUp.setOnClickListener {
             //Get details of User
@@ -47,44 +67,50 @@ class SignUp : AppCompatActivity() {
             val lastName = editLastName.text.toString()
             val email = editEmail.text.toString()
             val password = editPassword.text.toString()
+            var department = editDepartment.text.toString()
             val idNumber = (0..10000).random().toString() //Random number under 10,000
 
-            signUp(firstName, lastName, email, password, idNumber)
 
-            if(it is RadioButton){
-                val checked = it.isChecked
+            if (editDepartment.text.isEmpty() || editDepartment.text == null) {
+                department = ""
+            }
 
-                when(it.getId()){
-                    R.id.studentRadioButton ->
+            //Check to make sure all fields have input
+            val stringArray = arrayListOf(firstName, lastName, email, password)
 
-                        if(checked){
-                            title = "Student"
-                            editDepartment.clearFocus()
-                        }
+            for(string in stringArray){
 
-                    R.id.facultyRadioButton ->
-                        if(checked){
-                            title = "Faculty"
+                if(string == null  || string.isEmpty()){
 
-                        }
+                    when(string){
+                        firstName -> Toast.makeText(this, "Fill In Thw First Name Field",Toast.LENGTH_LONG).show()
+                        lastName ->   Toast.makeText(this, "Fill In The Last Name Field",Toast.LENGTH_LONG).show()
+                        email  ->   Toast.makeText(this, "Fill In The Email Field",Toast.LENGTH_LONG).show()
+                        password -> Toast.makeText(this, "Fill In The Password Field",Toast.LENGTH_LONG).show()
+                    }
                 }
             }
 
 
 
+            signUp(firstName, lastName, email, password, idNumber, role, department, year)
 
         }
 
     }
 
-    private fun signUp(firstName: String, lastName: String, email: String, password: String, idNumber: String) {
+
+
+
+    private fun signUp(firstName: String, lastName: String, email: String, password: String, idNumber: String,
+                            role: String, department: String, year: String) {
         //Logic for Signing up User
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     //code for jumping to home
 
-                    addUserToDatabase(firstName, lastName, email, password, idNumber, mAuth.currentUser?.uid!!)
+                    addUserToDatabase(firstName, lastName, email, password, idNumber, role, department, year, mAuth.currentUser?.uid!!)
                     val intent = Intent(this@SignUp,MainActivity::class.java)
                     finish()
                     startActivity(intent)
@@ -96,12 +122,14 @@ class SignUp : AppCompatActivity() {
             }
     }
 
-    private fun addUserToDatabase(firstName: String, lastName: String, email: String, password: String, idNumber: String, uid: String) {
+    private fun addUserToDatabase(firstName: String, lastName: String, email: String, password: String, idNumber: String,
+                                  role: String, department: String, year: String, uid: String) {
         //Initialize DB Reference
         mDBReference = FirebaseDatabase.getInstance().getReference()
 
         //"user" is parent node in path
-        mDBReference.child("user").child(uid).setValue(User(firstName, lastName, email, password, idNumber, uid))
+        mDBReference.child("user").child(uid).setValue(User(firstName, lastName, email, password, idNumber,
+            role, department, year, uid))
 
     }
 
--- app/src/main/java/com/ookie/polytechchat/User.kt
+++ app/src/main/java/com/ookie/polytechchat/User.kt
@@ -6,14 +6,9 @@ class User {
     var lastName: String? = null
     var email: String? = null
     var uID: String? = null
-
-    var facultyIDNumber: String? = null
     var idNumber: String? = null
     var department: String? = null
     var year: String? = null
-
-    var student: Boolean? = null
-    var facultyMember: Boolean? = null
     var role: String? = null //Only using if they are faculty
 
 
@@ -35,68 +30,31 @@ class User {
     }
 
     //If Student
-    constructor(firstName: String?, lastName: String?, email: String?, idNumber: String?, year: String?, uID: String?) {
+    constructor(firstName: String?, lastName: String?, email: String?, idNumber: String?, password: String?,
+                role: String?, department: String?, year: String?, uID: String?) {
         this.firstName = firstName
         this.lastName = lastName
         this.email = email
         this.idNumber = idNumber
+        this.role = role
+        this.department = department
         this.year = year
         this.uID = uID
     }
 
-
-    //Use this constructor if there is an image available for Student
-    constructor(firstName: String?, lastName: String?, email: String?, idNumber: String?, uID: String?, imageResourceID: Int) {
+    //If Image
+    constructor(firstName: String?, lastName: String?, email: String?, idNumber: String?,
+                role: String?, department: String?, year: String?, imageResourceId: Int, uID: String?) {
         this.firstName = firstName
         this.lastName = lastName
         this.email = email
         this.idNumber = idNumber
-        //this.year = year
-        this.uID = uID
-        this.mImageResourceID = imageResourceID
-    }
-
-
-
-/*
-    //If faculty
-    constructor(firstName: String?, lastName: String?, email: String?, facultyIdNumber: String, department: String, uID: String?) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.email = email
-        this.facultyIDNumber = facultyIdNumber
+        this.role = role
         this.department = department
+        this.year = year
+        this.mImageResourceID = imageResourceId
         this.uID = uID
     }
-*/
 
-    //Use this constructor if there is an image available for faculty
-    constructor(firstName: String?, lastName: String?, email: String?, facultyIdNumber: String, department: String, uID: String?, imageResourceID: Int) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.email = email
-        this.uID = uID
-        this.mImageResourceID = imageResourceID
-    }
-
-    //Use this constructor if there is a title but no image available
-    constructor(firstName: String?, lastName: String?, email: String?, uID: String?, role: String?) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.email = email
-        this.uID = uID
-        this.role = role
-    }
-
-
-    //Use this constructor if there is a title available
-    constructor(firstName: String?, lastName: String?, email: String?, uID: String?, imageResourceID: Int, role: String?) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.email = email
-        this.uID = uID
-        this.mImageResourceID = imageResourceID
-        this.role = role
-    }
 
 }
